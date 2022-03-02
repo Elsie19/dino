c6268d8ef0dbe438381e5313b56d4b892fa74ada
The TwilightBlood <hwengerstickel@protonmail.com>
2022-03-01 22:28:44 -0600

    verbose installer

--- install.sh
+++ install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+function msg() {
+	echo -e "\t==> $*"
+}
 set -e
 cd /tmp
 rm -rf dino || true
+msg "Cloning 'dino' to '/tmp'"
 git clone --depth=1 https://github.com/Henryws/dino.git
 cd dino
-sudo install -Dm755 dino /bin/dino
-sudo mkdir -p /usr/share/dino/programs
-sudo install -Dm755 programs/* -t /usr/share/dino/programs/
+msg "Installing 'dino' to '/bin/dino'"
+sudo install -v -Dm755 dino /bin/dino
+sudo mkdir -pv /usr/share/dino/programs
+sudo install -v -Dm755 programs/* -t /usr/share/dino/programs/
