089b94e378a6d152604a778056035ad2f6c5b144
The TwilightBlood <hwengerstickel@protonmail.com>
2022-03-01 21:07:00 -0600

    fix installer

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 cd /tmp
+rm -rf dino || true
 git clone --depth=1 https://github.com/Henryws/dino.git
 cd dino
-sudo install -dm755 dino /bin/dino
-sudo install -dm755 programs/* /usr/share/dino/programs
+sudo install -Dm755 dino /bin/dino
+sudo mkdir -p /usr/share/dino/programs
+sudo install -Dm755 programs/* -t /usr/share/dino/programs/
